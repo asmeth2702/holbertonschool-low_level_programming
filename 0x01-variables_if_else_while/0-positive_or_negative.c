28acfaccd1a4ee53348928b37cfb3bab37f4e778
Asmeth2702 <3827@holbertonschool.com>
2021-10-01 16:21:30 -0700

    add script

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,14 +9,15 @@
 int main(void)
 {
 int n;
-   
+
 srand (time (0));
 n = rand () - RAND_MAX / 2;
-if(n > 0) 
-printf ("%u is positive\n", n);
+if(n > 0)
+printf("%d is positive\n", n);
 else if(n < 0)
-printf ("%u is negative\n", n);
-else  
-printf ("%u is zero\n", n);
+printf("%d is negative\n", n);
+else
+printf("%d is zero\n", n);
+
 return (0);
 }
