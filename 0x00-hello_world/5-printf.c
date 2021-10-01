649ce2618c61b02bc2f559924226a11130816a59
Asmeth2702 <3827@holbertonschool.com>
2021-09-30 19:07:36 -0700

    add script

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * The function will output a string
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char *m = "with proper grammar, but the outcome is a piece of art,";
+
+printf("%s\n", m);
+
+return (0);
+
+}
