56e0a2a92008c019401326d22801cd057c302f2c
Asmeth2702 <3827@holbertonschool.com>
2021-09-30 18:49:08 -0700

    add script

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * The function will output a string
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char m = "\"Programming is like building a multilingual puzzle";
+
+puts(m);
+
+return (0);
+
+}
