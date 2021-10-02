56aae07a78d32f558f4ef1d237d88694a8e6a949
Asmeth2702 <3827@holbertonschool.com>
2021-10-01 20:10:26 -0700

    add script

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ *Return: Always 0 (success)
+ */
+int main(void)
+{
+int num = 0;
+
+while (num < 10)
+{
+printf("%d\n", num);
+num++;
+}
+
+return (0);
+}
