ccde4f75e359cccf503ae38627c8240495e5b6cb
Asmeth2702 <3827@holbertonschool.com>
2021-10-05 16:07:07 -0700

    add script

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - addition operation.
+ * @a: numbers to be added.
+ * @b: numbers to be addad.
+ * Return: result.
+ */
+int add(int a, int b)
+{
+int n;
+
+n = (a + b);
+return (n);
+}
